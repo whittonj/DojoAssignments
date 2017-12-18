85d2c83bf0cdabfa3233a11bb3b41ac7a92f0333
Jeff Whitton <whittonj@jeffwhitton.com>
2017-12-17 17:55:51 -0800

    table users assignment

--- WebFundamentals/Javascript/Users/script.js
+++ WebFundamentals/Javascript/Users/script.js
@@ -17,57 +17,14 @@ $(document).ready(function() {
     */
     $('form').submit(function(){
       var firstnm = $("input#fn").val();
+      var lstnm = $("input#ln").val();
+      var email = $("input#em").val();
+      var phn = $("input#ph").val();
       console.log(firstnm);
+      $("table#dataset").append("<tr><td>"+firstnm + "<td>" + lstnm + "<td>" + email + "<td>" + phn);
       return false;
-      
-      
     });
-    
-    
-
-      $( ".button[value ='Try html']").click(function() {
-        $( "div.demo" ).html("<h3>Goodbye world</h3>" ); 
-      });
-
-      $( ".button[value ='Try text']").click(function() {
-        $( "div.demo" ).text("<h3>Goodbye world</h3>" ); 
-      });
-
-      $( ".button[value ='Try Before']").click(function() {
-        $( "div.demo-before").before("+ + + " ); 
-      });
-
-      $( ".button[value ='Try After']").click(function() {
-        $( "div.demo-before").after("- - - " ); 
-      });
-
-      $( ".button[value ='Try attr']").click(function() {
-        var alt = $( "#cat" ).attr( "alt" );
-        $( "altthis" ).text( alt );
-      });
-
-      $( ".button[value ='Try val']").click(function() {
-        var alt = $( "#cat" ).attr( "alt" );
-        $( "input#valbox" ).val( "You tried .val" );
-      });
-
-      $( ".button[value ='Store Data']").click(function() {
-        $("body").data("First Item", 42 ); 
-        console.log("Data stored");
-      });
   
-      $( ".button[value ='Get Data']").click(function() {
-        console.log($("body").data()); 
-      });
-
-      
-
-      $( ".button[value ='Try Append']").click(function() {
-        $( "h1" ).append( "> " ); 
-      });
-
-    
-
-    });
+});
 
 
