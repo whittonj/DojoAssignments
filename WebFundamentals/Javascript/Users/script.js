abb7a4fed6a7556f8e5c128d3a928f7e78cd0da3
Jeff Whitton <whittonj@jeffwhitton.com>
2017-12-18 19:00:24 -0800

    Color picker

--- WebFundamentals/Javascript/Users/script.js
+++ WebFundamentals/Javascript/Users/script.js
@@ -1,20 +1,6 @@
 'use strict';
 $(document).ready(function() {
-    /*$('h1').css ("color", "red");*/
-    //$('.para1').hide(); /* .remove - completely removes it from page & code */
-    /*$('.para2').click (function() {
-        $("h1").css ("color", "#333333");
-    })
-    $('h2').click (function() {
-        $(this).fadeOut(5000, function(){ console.log("Done!")});
-        $("body").append("<h1>Hello stuff</h1");
-    })
-    $("body").on ("click", "h1", function(){ 
-        $("body").append("<h1>Hello stuff</h1");
-    })
-    $('h3').attr("class", "classname");
-    console.log($('h3').attr("class"));//
-    */
+ 
     $('form').submit(function(){
       var firstnm = $("input#fn").val();
       var lstnm = $("input#ln").val();
