e9128983c4b56c068cdb2e7b5270d07aa723292d
Jeff Whitton <whittonj@jeffwhitton.com>
2017-12-21 20:19:23 -0800

    Pokemon go!

---
+++ WebFundamentals/Javascript/pokemon/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function() {
+for (var i=1; i <=151; i++) { 
+$.get("https://pokeapi.co/api/v2/pokemon/"+i, function(response) {
+      console.log(response);
+var string = (response.sprites.front_default);
+console.log(string);
+$(".main_content").append("<img src = " + string + ">");
+
+});
+}
+    
+});
