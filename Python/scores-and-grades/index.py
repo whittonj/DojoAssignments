48ad7a49f86c9c13970a2abc7b95d2602e3f59f4
Jeff Whitton <whittonj@jeffwhitton.com>
2018-01-15 16:06:13 -0800

    Coin tosses

--- Python/scores-and-grades/index.py
+++ Python/scores-and-grades/index.py
@@ -1,19 +1,19 @@
-for i in range (1,10):
+heads = 0
+tails = 0
+result = ""
+for i in range (1,5000):
     import random
     random_num = random.random()
-    random_num = random_num*100
-    score = int(random_num)
-    if score <= 59:
-        grade = "F"
-    if score >59 and score <70:
-        grade = "D"
-    if score >69 and score <80:
-        grade = "C"
-    if score >79 and score <90:
-        grade = "B"
-    if score >= 90:
-        grade = "A"
+    score = round(random_num)
+    
+    if score == 0:
+        heads = heads + 1
+        result = "heads"
 
-    print "Score:", score, "; Your grade is", grade
-    print "End of program, bye!"
+    if score == 1:
+        tails = tails + 1
+        result = "tails"
+    
+    print "attempt #:", i, " - you got a ", result, "TOTAL: Heads -", heads, " | Tails - ", tails
+    
 
