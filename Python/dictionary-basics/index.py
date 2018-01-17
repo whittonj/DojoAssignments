7b8278a82d6ba8fd53397c3d52fb08f306de279c
Jeff Whitton <whittonj@jeffwhitton.com>
2018-01-17 10:53:13 -0800

    disctioary assignment

---
+++ Python/dictionary-basics/index.py
@@ -0,0 +1,12 @@
+mydict = {
+    "first-name": "Jeff",
+    "last-name": "Whitton",
+    "age": 45,
+    "country": "The United States of America",
+    "favorite-lang": "German",
+}
+
+print "My name is", mydict["first-name"],mydict["last-name"]
+print "My age is", mydict["age"]
+print "I was born in", mydict["country"]
+print "My favorite language is", mydict["favorite-lang"]
